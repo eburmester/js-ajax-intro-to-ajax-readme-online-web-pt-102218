44068b595afe5e13040a88eddc49a5b2418d613e
Elyse Burmester <burmesterelyse@gmail.com>
2019-06-14 16:59:36 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -21,5 +21,11 @@ function showRepositories() {
   document.getElementById('repositories').innerHTML = repoList;
 }
  
-
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showCommits);
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+  req.send();
+}
 
